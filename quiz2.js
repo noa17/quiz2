601e94c7f95d1b3c1087796d34110e9c6dbad6ab
Noah Abramson <noa17@pitt.edu>
2016-11-14 19:52:44 -0500

    Finished

--- quiz2.js
+++ quiz2.js
@@ -1,3 +1,5 @@
-consle.log('welocme to quiz town, population...YOU!')
 
-@('.dynamic').hmtl('<div p>HI THERE, I\'M FROM JAVASCRIPT</p></div>').fadeIn('slo')
+(function () {
+	console.log('welocme to quiz town, population...YOU!');
+	$('.dynamic').html('<div><p>HI THERE, I\'M FROM JAVASCRIPT</p></div>').fadeIn('slow');
+}) ();
